31e474fe1b264ed10fe6f07a0e72a958f2a5d7ca
Anurag Joshi <anuragajoshi@gmail.com>
2024-01-22 11:41:29 +0530

    Added setDepartment method

--- JavaProjects/src/emailApp/Email.java
+++ JavaProjects/src/emailApp/Email.java
@@ -1,5 +1,7 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -7,6 +9,23 @@ public class Email {
 	int mailboxCapacity;
 	private String alternamteEmail;
 	private String password;
-	private String test;
+	
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		setDepartment();
+	}
+	
+	private String setDepartment() {
+		System.out.println("Enter Department.\n1.Sales\n2.Development\n3.Accounting\n4.Others\n");
+		Scanner in = new Scanner(System.in);
+		int choice = in.nextInt();
+		switch(choice) {
+		case 1: return "Sales";
+		case 2: return "Development";
+		case 3: return "Accounting";
+		default: return "";
+		}
+	}
 
 }
--- JavaProjects/src/emailApp/EmailApp.java
+++ JavaProjects/src/emailApp/EmailApp.java
@@ -3,7 +3,8 @@ package emailApp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Email em1  = new Email("Anurag", "Joshi");
 
 	}
 
