444e7de1220df842ac22dae85c9f460847077be8
Anurag Joshi <anuragajoshi@gmail.com>
2024-01-22 11:20:12 +0530

    init

---
+++ JavaProjects/src/emailApp/Email.java
@@ -0,0 +1,11 @@
+package emailApp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String department;
+	int mailboxCapacity;
+	private String alternamteEmail;
+	private String password;
+
+}
